33842f4f536f9f7a30db6b58235e18b732b5e393
Jon St John <jonstjohn@gmail.com>
2013-09-28 22:23:39 +0000

    Working on graph directive, data service

--- static/js/acng.js
+++ static/js/acng.js
@@ -34,6 +34,12 @@ angular.module("acApp").factory("siteService", function(){
   return {sharedObject: {data: null } }
 });
 
+angular.module("acApp").factory('dataService', function() {
+
+    return { sharedObject: { data: null } };
+
+});
+
 function MainCntl($scope, $http, siteService) {
 
     $scope.sites = siteService.sharedObject.data;
@@ -60,13 +66,13 @@ function HomeCntl($scope, $route, $routeParams, $location, siteService) {
     $scope.$routeParams = $routeParams;
 }
 
-function SiteCntl($scope, $route, $routeParams, $http, $location, siteService) {
+function SiteCntl($scope, $route, $routeParams, $http, $location, siteService, dataService) {
 
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
 
-    $scope.data = [];
+    $scope.data = dataService.sharedObject.data;
     $scope.sites = siteService.sharedObject.data;
     $scope.forecast = [];
 
@@ -83,13 +89,13 @@ function SiteCntl($scope, $route, $routeParams, $http, $location, siteService) {
     $scope.$watch('code', function() { $scope.setSiteName(); $scope.loadData(); });
 
     $scope.loadData = function() {
-        $scope.data = [];
+        siteService.sharedObject.data = [];
         $location.path('/site/' + $scope.code);
         var httpRequest = $http({
             method: 'GET',
             url: '/api/site/' + $scope.code + '/10'
         }).success(function(data, status) {
-            $scope.data = data.data;
+            siteService.sharedObject.data = data.data;
             $scope.forecast = data.forecast;
         });
     };
@@ -120,13 +126,23 @@ function ContactCntl($scope) {
 
 }
 
-app.directive('sampleGraph', function() {
+app.directive('sampleGraph', function(dataService) {
 
     // constants
     var margin = 20,
         width = 960,
         height = 500 - .5 - margin,
         color = d3.interpolateRgb("#f77", "#77f");
+
+    // create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
+    var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7];
+
+    // X scale will fit all values from data[] within pixels 0-w
+    var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
+    // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+    var y = d3.scale.linear().domain([0, 10]).range([height, 0]);
+        // automatically determining max range can work something like this
+        // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
 
     return {
         restrict: 'E',
@@ -140,7 +156,16 @@ app.directive('sampleGraph', function() {
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height + margin + 100);
+            // A label for the current year.
+            var title = vis.append("text")
+                .attr("class", "title")
+                .attr("dy", ".71em")
+                .text(2000);
 
+            vis.selectAll("rect").data(data).enter().append('rect')
+                .attr('y', function(d, i) { return i * 20 })
+                .attr('width', x)
+                .attr('height', 20);
 
             scope.$watch('exp', function(newVal, oldVal) {
 
