447d2a7b8bc9b991832025dca47c46764bd53d84
Jon St John <jonstjohn@gmail.com>
2014-07-26 04:23:08 +0000

    Deploy to production

--- aircanary/deploy/utils.py
+++ aircanary/deploy/utils.py
@@ -5,12 +5,12 @@ from django.core.management import call_command
 def deploy_prod():
 
     import subprocess
-    print(subprocess.check_output(['git', '-C', '/home/ac/air-canary-web', 'reset', '--hard', 'HEAD'])
-    print(subprocess.check_output(['git', '-C', '/home/ac/air-canary-web', 'pull'])
-    print(subprocess.check_output(['/home/ac/Envs/ac/bin/pip', 'install', '-r', '/home/ac/air-canary-web/requirements/production.txt'])
+    print(subprocess.check_output(['git', '-C', '/home/ac/air-canary-web', 'reset', '--hard', 'HEAD']))
+    print(subprocess.check_output(['git', '-C', '/home/ac/air-canary-web', 'pull']))
+    print(subprocess.check_output(['/home/ac/Envs/ac/bin/pip', 'install', '-r', '/home/ac/air-canary-web/requirements/production.txt']))
 
     call_command('migrate')
     call_command('collectstatic', interactive=False)
 
-    print(subprocess.check_output(['sudo', 'service', 'uwsgi', 'restart'])
-    print(subprocess.check_output(['sudo', 'service', 'nginx', 'restart'])
+    print(subprocess.check_output(['sudo', 'service', 'uwsgi', 'restart']))
+    print(subprocess.check_output(['sudo', 'service', 'nginx', 'restart']))
