ade4e95911c6f8e8b375ae2ae0c764036c892eb1
Jon St John <jonstjohn@gmail.com>
2014-11-06 23:42:11 +0000

    Desktop and mobile cleanup, geoPosition.js added back

--- aircanary/places/views.py
+++ aircanary/places/views.py
@@ -15,12 +15,20 @@ class MobileView(TemplateView):
 
     def get_context_data(self, **kwargs):
 
-        context = super(MobileView, self).get_context_data(**kwargs)
-        lat = float(self.request.GET.get('lat', 40.7500))
-        lon = float(self.request.GET.get('lon', -111.8833))
-
         from places.models import Place
-        p = Place(lat, lon)
+
+        context = super(MobileView, self).get_context_data(**kwargs)
+
+        if self.request.GET.get('srch'):
+            from urllib import unquote
+            srch = unquote(self.request.GET.get('srch'))
+            p = Place.from_name(srch)
+            context['srch'] = srch
+        else:
+            lat = float(self.request.GET.get('lat', 40.7500))
+            lon = float(self.request.GET.get('lon', -111.8833))
+            p = Place(lat, lon)
+
         context['place'] = p
 
         return context
